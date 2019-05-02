6ed80df35eae08996c1fae84ba296e0f7ae69b12
misstonbon <plusdeltah@gmail.com>
2019-05-01 19:09:20 -0700

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static String james = "James";
+    static int jamesIsAFive = 5;
+
+    public static void main(String[] args) {
+        System.out.println("Hello " + james + ", you're hot!, I  give you a 5/" + jamesIsAFive);
+    }
+
+}
